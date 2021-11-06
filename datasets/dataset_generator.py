a21e6690c5b13388d4b293a6220f090702274864
Juan Neri <juanjo.neri@gmail.com>
2021-11-06 17:28:03 -0500

    create dataset that is completely random

--- datasets/dataset_generator.py
+++ datasets/dataset_generator.py
@@ -4,6 +4,7 @@ from scipy.spatial import ConvexHull
 
 import os
 import networkx as nx
+import numpy as np
 
 def _build_graph(nodes, edges=[]):
     '''
@@ -19,19 +20,27 @@ def _build_graph(nodes, edges=[]):
         G.add_edge(edges[-1], edges[0])
     return G
 
-def _init_blobs(n_nodes, centers, std):
-    nodes, _ = datasets.make_blobs(n_samples=n_nodes, centers=centers, cluster_std=std)
+def _init(nodes):
     edges = ConvexHull(nodes).vertices
-
     coordinates = dict(list(enumerate(map(tuple, nodes))))
     return _build_graph(nodes, edges), coordinates, edges
+
+
+def _init_blobs(n_nodes, centers, std):
+    nodes, _ = datasets.make_blobs(n_samples=n_nodes, centers=centers, cluster_std=std)
+    return _init(nodes)
+
+def _init_rand(n_nodes):
+    nodes = np.random.rand(n_nodes, 2)
+    return _init(nodes)
 
 def _init_halfmoons(n_nodes, noise):
     nodes, _  = datasets.make_moons(n_samples=n_nodes, noise=noise)
-    edges = ConvexHull(nodes).vertices
+    return _init(nodes)
 
-    coordinates = dict(list(enumerate(map(tuple, nodes))))
-    return _build_graph(nodes, edges), coordinates, edges
+def _init_circles(n_nodes, noise):
+    nodes, _  = datasets.make_circles(n_samples=n_nodes, noise=noise)
+    return _init(nodes)
 
 def _plot_graph(G, coordinates, output_file):
     os.makedirs(output_file, exist_ok=True)
@@ -61,12 +70,20 @@ def _save_dataset(G, coordinates, edges, output_file):
 if __name__ == '__main__':
 
     for size in (10, 100, 1000, 5000, 10000):
+        # Rand
+        G, coordinates, edges = _init_rand(size)
+        _save_dataset(G, coordinates, edges, f'rand-{size}')
+
+        # Circle
+        G, coordinates, edges = _init_circles((size//2, size//2), 0.05)
+        _save_dataset(G, coordinates, edges, f'circle-{size}')
+
         # Cluster
-        G, coordinates, edges = _init_blobs(size, [(5,5)], 0.2)
+        G, coordinates, edges = _init_blobs(size, [(0.5, 0.5)], 0.2)
         _save_dataset(G, coordinates, edges, f'cluster-{size}')
 
         # Blob
-        G, coordinates, edges = _init_blobs(size, [(0,0), (1,1)], 0.3)
+        G, coordinates, edges = _init_blobs(size, [(0,0), (0,1), (1,1)], 0.15)
         _save_dataset(G, coordinates, edges, f'blobs-{size}')
 
         # Moon
