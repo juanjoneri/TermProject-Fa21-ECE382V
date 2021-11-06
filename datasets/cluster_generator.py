6f2c2fbaeab0cdad4c2ff122f167eb5cff3a979d
Juan Neri <juanjo.neri@gmail.com>
2021-11-06 13:39:06 -0500

    Add ability to plot generated datasets

--- datasets/cluster_generator.py
+++ datasets/cluster_generator.py
@@ -1,4 +1,5 @@
 from sklearn import datasets
+from matplotlib import pyplot
 
 import networkx as nx
 import numpy as np
@@ -15,6 +16,14 @@ def _build_graph(nodes, edges=[]):
         G.add_edge(x, y)
     return G
 
+def _plot_graph(G, coordinates, output_file):
+    fig = pyplot.figure()
+    fig.add_subplot(1,1,1)
+    pyplot.axes([0, 0, 1, 1])
+    nx.draw(G, coordinates, with_labels=True)
+    pyplot.savefig(output_file)
+
+
 def _init_blobs(n_nodes, centers, std):
     nodes, _ = datasets.make_blobs(n_samples=n_nodes, centers=centers, cluster_std=std)
     coordinates = dict(list(enumerate(map(tuple, nodes))))
@@ -26,5 +35,8 @@ def _init_halfmoons(n_nodes, noise):
     return _build_graph(nodes), coordinates
 
 if __name__ == '__main__':
-    G, coordinates = _init_blobs(20, [(0,0), (1,1)], 0.5)
-    print(coordinates)
+    G, coordinates = _init_blobs(20, [(0,0), (1,1)], 0.2)
+    _plot_graph(G, coordinates, 'blobs.png')
+
+    G, coordinates = _init_halfmoons(20, 0.1)
+    _plot_graph(G, coordinates, 'moon.png')
