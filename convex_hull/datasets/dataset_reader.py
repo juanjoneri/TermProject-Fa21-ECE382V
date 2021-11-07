5cdd86854a7bedbce249e185e58060fdd7ba865a
Juan Neri <juanjo.neri@gmail.com>
2021-11-06 20:44:12 -0500

    Implement naive algo

--- convex_hull/datasets/dataset_reader.py
+++ convex_hull/datasets/dataset_reader.py
@@ -1,14 +1,33 @@
 import pandas as pd
 
+class DatasetReader():
 
-def import_dataset(dataset_name):
-    df = pd.read_csv(f'{dataset_name}/input.csv', header=None)
-    data = list(df.itertuples(index=False, name=None))
-    with open(f'{dataset_name}/solution.txt') as f:
-        solution = list(map(int, f.read().split(',')))
+    def __init__(self, dataset):
+        self.data = DatasetReader._read_data(dataset)
+        self.solution = DatasetReader._read_solution(dataset)
 
-    return data, solution
+    def check_solution(self, edges):
+        actual = {tuple(sorted(edge)) for edge in edges}
+        expected = {tuple(sorted(edge)) for edge in self.solution}
+        assert(actual == expected)
+
+    def _read_data(dataset):
+        df = pd.read_csv(f'{dataset}/input.csv', header=None)
+        data = list(df.itertuples(index=False, name=None))
+        return data
+    
+    def _read_solution(dataset):
+        solution = []
+        with open(f'{dataset}/solution.txt') as f:
+            solution_nodes = list(map(int, f.read().split(',')))
+            for edge in zip(solution_nodes, solution_nodes[1:]):
+                solution.append(edge)
+            solution.append((solution_nodes[-1], solution_nodes[0]))
+        return solution
+
 
 if __name__ == '__main__':
-    d, s = import_dataset('blobs-100')
+    dataset_reader = DatasetReader('blobs-10')
+    d = dataset_reader.data
+    s = dataset_reader.solution
     print(d, s)
--- convex_hull/naive.py
+++ convex_hull/naive.py
@@ -1,8 +1,29 @@
 from itertools import combinations
-from datasets.dataset_reader import import_dataset
+from datasets.dataset_reader import DatasetReader
+
+import numpy as np
 
 def _is_in_hull(edge, vertices):
+    '''
+    Check that the cross product of the given edge with all other vertices has the same sign
+    '''
+    a, b = edge
+    v = np.subtract(b, a)
+    sign = None
+    for c in vertices:
+        w = np.subtract(c, a)
+        cross = np.cross(v, w)
+        current_sign = (cross > 0)
+        if (cross == 0):
+            continue
+        elif sign is None:
+            sign = current_sign
+        elif sign != current_sign:
+            return False
+    
     return True
+
+
 
 def compute(vertices):
     indices = {v: i  for i, v in enumerate(vertices)}
@@ -12,8 +33,9 @@ def compute(vertices):
             a, b = edge
             hull.add((indices[a], indices[b]))
     
-    print(hull)
+    return hull
 
 if __name__ == '__main__':
-    data, solution = import_dataset('datasets/blobs-10')
-    compute(data)
+    reader = DatasetReader('datasets/blobs-10')
+    solution = compute(reader.data)
+    print(reader.check_solution(solution))
