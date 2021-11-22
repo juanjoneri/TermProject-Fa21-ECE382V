c47c8ea61868b250a08c377a72ec730e6dcd4a30
Juan Neri <juanjo.neri@gmail.com>
2021-11-22 17:14:08 -0600

    rename one var

--- convex_hull/approx_quickhull.py
+++ convex_hull/approx_quickhull.py
@@ -8,17 +8,17 @@ class ApproxQuickHull(QuickHull):
     
     def _compute(self):
         # 1. Divide the x axis into buckets of size "error"
-        left, right = min(self._vertices), max(self._vertices)
-        bucketCount = ceil((right[0] - left[0]) / self._error)
-        buckets = [[(None, float('inf')), (None, float('-inf'))] for _ in range(bucketCount)]
+        (left, _), (right, _) = min(self._vertices), max(self._vertices)
+        bucket_count = ceil((right - left) / self._error)
+        buckets = [[(None, float('inf')), (None, float('-inf'))] for _ in range(bucket_count)]
         
         # 2. Keep track of the points in each bucket with largest and smallest y values
-        for vertex in self._vertices:
-            bucket_index = int((vertex[0] - left[0]) // self._error)
-            if vertex[1] > buckets[bucket_index][1][1]:
-                buckets[bucket_index][1] = vertex
-            if vertex[1] < buckets[bucket_index][0][1]:
-                buckets[bucket_index][0] = vertex
+        for x, y in self._vertices:
+            bucket_index = int((x - left) // self._error)
+            if y > buckets[bucket_index][1][1]:
+                buckets[bucket_index][1] = (x, y)
+            if y < buckets[bucket_index][0][1]:
+                buckets[bucket_index][0] = (x, y)
         
         # 3. Use the elements found in each bucket as the new vertices to solve quickhull
         self._vertices = list({ vertex for bucket in buckets for vertex in bucket if vertex[0] is not None })
