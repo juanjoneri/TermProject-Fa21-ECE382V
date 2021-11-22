d1d245229d8fd30fc0e263f25214277d47c9e4af
Juan Neri <juanjo.neri@gmail.com>
2021-11-21 22:10:29 -0600

    use multiprocessing instead of multithreading

--- convex_hull/parallelizedquickhull.py
+++ convex_hull/parallelizedquickhull.py
@@ -1,7 +1,7 @@
 from algorithm import Algorithm
 from datasets.dataset_reader import Dataset
 from math import log
-from threading import Thread
+from multiprocessing import Process, Queue
 import networkx as nx
 
 import sys 
@@ -13,26 +13,28 @@ class ParallelizedQuickHull(Algorithm):
         upper = ParallelizedQuickHull._divide_vertices(edge, self._vertices, 1)
         lower = ParallelizedQuickHull._divide_vertices(edge, self._vertices, -1)
 
+        q1, q2 = Queue(), Queue()
         if self.cores >= 2:
-            hull1, hull2 = nx.Graph(), nx.Graph()
-            t1 = Thread(target=self._quickhull, args=(edge, upper, hull1))
-            t2 = Thread(target=self._quickhull, args=(edge, lower, hull2))
+            t1 = Process(target=self._quickhull, args=(edge, upper, q1))
+            t2 = Process(target=self._quickhull, args=(edge, lower, q2))
             t1.start()
             t2.start()
             t1.join()
             t2.join()
         else:
             hull1 = hull2 = nx.Graph()
-            self._quickhull(edge, upper, hull1)
-            self._quickhull(edge, lower, hull2)
+            self._quickhull(edge, upper, q1)
+            self._quickhull(edge, lower, q2)
         
-        hull = nx.compose(hull1, hull2)
+        hull = nx.compose(q1.get(), q2.get())
         return Algorithm._to_deque(hull)
 
 
-    def _quickhull(self, edge, vertices, hull, depth=2):
+    def _quickhull(self, edge, vertices, q, depth=2):
+        hull = nx.Graph()
         if not any(vertices):
             self._add_to_hull(hull, edge)
+            q.put(hull)
             return
         
         furthest = ParallelizedQuickHull._find_futhest(edge, vertices)
@@ -43,19 +45,22 @@ class ParallelizedQuickHull(Algorithm):
         vertices1 = ParallelizedQuickHull._divide_vertices(edge1, vertices, sign1)
         vertices2 = ParallelizedQuickHull._divide_vertices(edge2, vertices, sign2)
 
+        q1, q2 = Queue(), Queue()
         if (log(self.cores, 2) >= depth):
-            hull1, hull2 = nx.Graph(), nx.Graph()
-            t1 = Thread(target=self._quickhull, args=(edge1, vertices1, hull1, depth+1))
-            t2 = Thread(target=self._quickhull, args=(edge2, vertices2, hull2, depth+1))
+            t1 = Process(target=self._quickhull, args=(edge1, vertices1, q1, depth+1))
+            t2 = Process(target=self._quickhull, args=(edge2, vertices2, q2, depth+1))
             t1.start()
             t2.start()
             t1.join()
             t2.join()
-            hull.update(hull1.edges(), hull1.nodes())
-            hull.update(hull2.edges(), hull2.nodes())
         else:
-            self._quickhull(edge1, vertices1, hull, depth+1)
-            self._quickhull(edge2, vertices2, hull, depth+1)
+            self._quickhull(edge1, vertices1, q1, depth+1)
+            self._quickhull(edge2, vertices2, q2, depth+1)
+        
+        hull1, hull2 = q1.get(), q2.get()
+        hull.update(hull1.edges(), hull1.nodes())
+        hull.update(hull2.edges(), hull2.nodes())
+        q.put(hull)
 
         
     @classmethod
